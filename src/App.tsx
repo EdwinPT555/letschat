50f382b7b9c7198009d31af734e5d784860cd6b6
Edwin <edwin.pt@mydock365.com>
2022-10-16 22:46:15 +0530

    bottom nav added

--- src/App.tsx
+++ src/App.tsx
@@ -1,108 +1,14 @@
-import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-import SearchIcon from '@mui/icons-material/Search';
-import MoreIcon from '@mui/icons-material/MoreVert';
+import React from 'react';
+import SubNav from './component/SubNav';
+import TopNav from './component/TopNav';
 
-const LeftWrapper = styled('div')(({ theme }) => ({
-  width: '50%',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-start',
-}));
-const RightWrapper = styled('div')(({ theme }) => ({
-  width: '50%',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-end',
-}));
-
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  // width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(1),
-    width: 'auto',
-  },
-}));
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
-      },
-    },
-  },
-}));
-
-export default function App() {
+const App = () => {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <LeftWrapper>
-            <img src="https://www.pngkey.com/png/full/372-3728292_message-ios-icon-png.png" alt="" style={{ height: "30px" }} />
-            <Typography
-              variant="h5"
-              noWrap
-              component="div"
-              sx={{ marginLeft: "10px" }}
-            >
-              LetsChat
-            </Typography>
-          </LeftWrapper>
-          <RightWrapper>
-            <Search>
-              <SearchIconWrapper>
-                <SearchIcon />
-              </SearchIconWrapper>
-              <StyledInputBase
-                placeholder="Search…"
-                inputProps={{ 'aria-label': 'search' }}
-              />
-            </Search>
-            <IconButton
-              size="large"
-              aria-label="display more actions"
-              edge="end"
-              color="inherit"
-            >
-              <MoreIcon />
-            </IconButton>
-          </RightWrapper>
-
-
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+    <div>
+      <TopNav />
+      <SubNav />
+    </div>
+  )
 }
+
+export default App
---
+++ src/component/TopNav.tsx
@@ -0,0 +1,108 @@
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+import MoreIcon from '@mui/icons-material/MoreVert';
+
+const LeftWrapper = styled('div')(({ theme }) => ({
+  width: '50%',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-start',
+}));
+const RightWrapper = styled('div')(({ theme }) => ({
+  width: '50%',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+}));
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  // width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
+
+export default function TopNav() {
+  return (
+    <Box sx={{ flexGrow: 1, position: "sticky", top: 0, zIndex: 10 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <LeftWrapper>
+            <img src="https://www.pngkey.com/png/full/372-3728292_message-ios-icon-png.png" alt="" style={{ height: "30px" }} />
+            <Typography
+              variant="h5"
+              noWrap
+              component="div"
+              sx={{ marginLeft: "10px" }}
+            >
+              LetsChat
+            </Typography>
+          </LeftWrapper>
+          <RightWrapper>
+            <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Search…"
+                inputProps={{ 'aria-label': 'search' }}
+              />
+            </Search>
+            <IconButton
+              size="large"
+              aria-label="display more actions"
+              edge="end"
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+          </RightWrapper>
+
+
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
